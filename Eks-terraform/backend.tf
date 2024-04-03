ead4f6d3432d1d79af6879cf078c5facef51ca4a
gattupallisai <131510816+gattupallisai@users.noreply.github.com>
2024-04-03 10:16:29 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "minibucket01" # Replace with your actual S3 bucket name
-    key    = "EKS_terraform.tfstate"
+    key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
 }
